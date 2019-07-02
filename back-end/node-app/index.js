a6e162a7f6113a1845b67b9a4959ecad14772da2
Jake Lemar <jalema01@wsc.edu>
2019-07-01 20:03:52 -0500

    readded backend

--- back-end/node-app/index.js
+++ back-end/node-app/index.js
@@ -1,7 +1,36 @@
-const express = require('express')
-const app = express()
+
+
+const app = require('express')();
 const port = 5000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+var expressMongoDb = require('express-mongo-db');
+app.use(expressMongoDb('mongodb://mongodb:27017/mydb'));
+
+
+app.get('/companies', getCompanies);
+
+app.get('/addCompany', addCompany);
 
 app.listen(process.env.PORT || 5000)
+
+function getCompanies (req, res) {
+    var dbo = req.db.db("mydb");
+
+    dbo.collection('customers').find().toArray( function(err, results) {
+        res.send(results)
+    });
+
+    req.db.close();
+};
+
+function addCompany (req, res) {
+    var dbo = req.db.db("mydb");
+
+    var myobj = { name: "Company LLC", address: "Highway 9" };
+
+    dbo.collection("customers").insertOne(myobj, function(err, response) {
+        if (err) throw err;
+        res.send("1 document inserted");
+        req.db.close();
+    });
+} 
