3e352f1177fce352c43139f12aee4646dcad13a2
Jake Lemar <jakelemar98@Jakes-MacBook-Pro.local>
2019-07-03 13:37:52 -0500

    changes

--- back-end/node-app/index.js
+++ back-end/node-app/index.js
@@ -4,30 +4,33 @@ const port = 5000
 var expressMongoDb = require('express-mongo-db');
 app.use(expressMongoDb('mongodb://mongodb:27017'));
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/companies', getCompanies);
 
-app.get('/addCompany', addCompany);
+app.post('/addCompany', addCompany);
 
 app.listen(process.env.PORT || 5000)
 
 function getCompanies (req, res) {
     var dbo = req.db.db("mydb");
-
+    
     dbo.collection('customers').find().toArray( function(err, results) {
         res.send(results)
     });
 
 };
 
-function addCompany (req, res) {
+function addCompany (req, res) {    
     var dbo = req.db.db("mydb");
 
-    var myobj = { name: "Company LLC", address: "Highway 10" };
+    var myobj = { name: req.body.name, address: req.body.message };
 
     dbo.collection("customers").insertOne(myobj, function(err, response) {
         if (err) throw err;
-        res.send("1 document inserted");
+        res.send(response);
     });
 };
 
