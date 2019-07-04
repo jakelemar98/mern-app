d29a00e7f36bf98f69896d62dbff9aa53297a85b
Jake Lemar <jakelemar98@Jakes-MacBook-Pro.local>
2019-07-04 17:28:20 -0500

    changes

--- front-end/react-app/src/App.js
+++ front-end/react-app/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> de
+          Edit <code>src/App.js</code> deployed
         </p>
         <a
           className="App-link"
