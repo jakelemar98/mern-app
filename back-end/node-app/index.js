296eabb2d3ec3bbe728431255364060cf40dae93
Jake Lemar <jakelemar98@Jakes-MacBook-Pro.local>
2019-07-04 22:34:06 -0500

    k8s

--- back-end/node-app/index.js
+++ back-end/node-app/index.js
@@ -2,7 +2,7 @@ const app = require('express')();
 const port = 5000
 
 var expressMongoDb = require('express-mongo-db');
-app.use(expressMongoDb('mongodb://mongodb:27017'));
+app.use(expressMongoDb('mongodb+srv://jakelemar98:<password>@appcluster-btfvs.mongodb.net/test?retryWrites=true&w=majority'));
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -15,20 +15,20 @@ app.post('/addCompany', addCompany);
 app.listen(process.env.PORT || 5000)
 
 function getCompanies (req, res) {
-    var dbo = req.db.db("mydb");
+    var dbo = req.db.db("test");
     
-    dbo.collection('customers').find().toArray( function(err, results) {
+    dbo.collection('companies').find().toArray( function(err, results) {
         res.send(results)
     });
 
 };
 
 function addCompany (req, res) {    
-    var dbo = req.db.db("mydb");
+    var dbo = req.db.db("app");
 
     var myobj = { name: req.body.name, address: req.body.message };
 
-    dbo.collection("customers").insertOne(myobj, function(err, response) {
+    dbo.collection("companies").insertOne(myobj, function(err, response) {
         if (err) throw err;
         res.send(response);
     });
