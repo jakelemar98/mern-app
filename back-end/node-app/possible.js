b32094cf30521f38bea6837a881bf823c3164578
jakelemar98 <30849493+jakelemar98@users.noreply.github.com>
2019-07-05 10:25:02 -0500

    added JWT (#10)

--- back-end/node-app/possible.js
+++ back-end/node-app/possible.js
@@ -7,3 +7,28 @@ client.connect(err => {
   // perform actions on the collection object
   client.close();
 });
+
+app.post("/register",  (req, res) => {
+  try {
+      req.body.password = Bcrypt.hashSync(req.body.password, 10);
+      var dbo = req.db.db("app");
+      
+      var myobj = {
+              name: req.body.name,
+              user_id: req.body.user_id,
+              username: req.body.username,
+              password: req.body.password
+          };
+  
+      dbo.collection("users").insertOne(myobj, function(err, response) {
+          if (err) throw err;
+          res.json({
+              response
+          });
+      });
+  } catch (error) {
+      console.log(error);
+      
+      res.sendStatus(500);
+  }
+});
