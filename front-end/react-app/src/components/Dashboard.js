7b5594a3c1cc14f3ce2f36a48c8bd7d940efba9a
Jake Lemar <jakelemar98@Jakes-MacBook-Pro.local>
2019-07-08 13:40:22 -0500

    changes

--- front-end/react-app/src/components/Dashboard.js
+++ front-end/react-app/src/components/Dashboard.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react'
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
 import Nav from './Nav'
+import gitImg from '../static/images/github.svg'; 
+
 
 export class Dashboard extends Component {
     constructor(props) {
         super(props);
         this.state = {
-          error: null,
-          isLoaded: false,
-          items: []
+          dashError: null,
+          gitError: null,
+          dashIsLoaded: false,
+          gitIsLoaded: false,
+          items: [],
+          gitItems: [],
+          baseGitUrl: "https://github.com/jakelemar98/mern-app/tree/"
         };
       }
     
       componentDidMount() {
+        this.fetchDashItems();
+        this.fetchGitRepos();
+      }
+      
+      fetchDashItems = e => {
         var url = process.env.REACT_APP_API_URI + 'companies';
 
         var token = localStorage.getItem('token');
@@ -25,25 +41,54 @@ export class Dashboard extends Component {
           .then(res => res.json())
           .then( (result) => {
                 this.setState({
-                    isLoaded: true,
+                    dashIsLoaded: true,
                     items: result.results
                 });                
             },
             (error) => {
               this.setState({
-                isLoaded: true,
-                error
+                dashIsLoaded: true,
+                dashError: error
               });
             }
           )
       }
-    
-      render() {
-        const { error, isLoaded, items } = this.state;
+
+      fetchGitRepos = e => {
+        var url = "https://api.github.com/repos/jakelemar98/mern-app/branches"
+
+        fetch(url, {
+            method: "GET"
+        })
+          .then(res => res.json())
+          .then( (result) => {
+              console.log(result);
+              
+              this.setState({
+                  gitIsLoaded: true,
+                  gitItems: result
+              });                
+            },
+            (error) => {
+              this.setState({
+                gitIsLoaded: true,
+                gitError: error
+              });
+            }
+          )
+      }
+
+      goToBranch = (e, branch) => {
+        console.log(branch);
         
-        if (error) {
-          return <div>Error: {error.message}</div>;
-        } else if (!isLoaded) {
+      }
+
+      render() {
+        const { error, items, gitItems,dashError, gitError, dashIsLoaded, gitIsLoaded, baseGitUrl } = this.state;
+        
+        if (dashError || gitError) {
+          return <div>Dash Error: {error.message} Git Error: {gitError.message}</div>;
+        } else if (!dashIsLoaded || !gitIsLoaded) {
           return <div>Loading...</div>;
         } else {
             return (
@@ -56,10 +101,63 @@ export class Dashboard extends Component {
                         </li>
                     ))}
                 </ul>
+                <br></br>
+                <div>
+                  <Grid container spacing={3}>
+                  {gitItems.map((item) => (
+                    <Grid item xs={4} key={item.name}>
+                      <a href={baseGitUrl + item.name} >
+                        <Card style={ styles.card } >
+                          <div style={styles.details}>
+                            <CardContent style={styles.content}>
+                              <Typography component="h5" variant="h5">
+                                {item.name}
+                              </Typography>
+                              <Typography variant="subtitle1" color="textSecondary">
+                                mern-app
+                              </Typography>
+                            </CardContent>
+                          </div>
+                          <CardMedia
+                            style={styles.cover}
+                            image={gitImg}
+                            title="Live from space album cover"
+                          />
+                        </Card>
+                      </a>
+                    </Grid>
+                  ))}
+                  </Grid>
+                </div>
               </div>
+               
             )
         }
     }
+}
+
+const styles = {
+  card: {
+    display: 'flex',
+  },
+  details: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  content: {
+    flex: '1 0 auto',
+  },
+  cover: {
+    width: 151,
+  },
+  controls: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  playIcon: {
+    height: 38,
+    width: 38,
+  },
 }
 
 export default Dashboard
