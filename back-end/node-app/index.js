7d56fc42103a744521f9349ecaee0a70bed22731
jakelemar98 <30849493+jakelemar98@users.noreply.github.com>
2019-07-24 23:51:34 -0500

    Test pr (#33)
    * changes
    
    * changes

--- back-end/node-app/index.js
+++ back-end/node-app/index.js
@@ -28,7 +28,7 @@ app.get('/api/todos', verifyToken, getTodos);
 app.post('/api/addTodo', verifyToken, addTodo);
 
 app.get("/api/unitTest", (req, res) => {
-    res.send({"Hey": "hello"})
+    res.send({"Hey": "helo"})
 })
 
 app.listen(process.env.PORT || 5000)
