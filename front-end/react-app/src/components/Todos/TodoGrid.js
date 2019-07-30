a2cc12ed1b3d3beede1e14c4dd5ed85295aa27fb
Jake Lemar <jalema01@wsc.edu>
2019-07-29 20:01:50 -0500

    have sort working

--- front-end/react-app/src/components/Todos/TodoGrid.js
+++ front-end/react-app/src/components/Todos/TodoGrid.js
@@ -52,14 +52,23 @@ const styles = {
 
   const updatedValues = []
 
+  var allTodos = [];
+
 export default function TodoGrid(props) {    
     const {todos, users} = props;    
     const [open, setOpen] = React.useState(false);
     const [data, setData] = React.useState({});
     const [disabled, setDisabled] = React.useState();
     const [add, setAdd] = React.useState()
+    const [status, setStatus] = React.useState("")
+    const [worker, setWorker] = React.useState(0)
+    const [priority, setPriority] = React.useState("")
+
+    allTodos = todos;
 
     function handleClickOpen(index) {
+        console.log(index);
+        
         setData(todos[index])
         setAdd(false)
         setDisabled(true)
@@ -73,8 +82,16 @@ export default function TodoGrid(props) {
     const myCallback = (key, data) => {
         key = key.toLowerCase()
         updatedValues[key] = data;
-        console.log(updatedValues);
-            
+        if (key === "status") {
+            console.log("status");
+            setStatus(data)
+        } else if (key === "sugg_worker") {
+            console.log("worker");
+            setWorker(data)
+        } else {
+            console.log(priority);
+            setPriority(data)
+        }
     }
 
     function handleNew(){
@@ -84,28 +101,61 @@ export default function TodoGrid(props) {
         setOpen(true)
     }
 
-    function CardFill(){
+    function CardFill(vals){        
+        var data = todos.filter(function(todo, index){
+            todo.index = index;
+            var trueStatus, trueWorker, truePriority = false
+            
+            if  (vals.status !== ""){
+                trueStatus = todo.status === vals.status;
+            } else {
+                trueStatus = true;
+            }
+
+            if (vals.sugg_worker !== 0){
+                trueWorker = todo.sugg_worker === vals.sugg_worker;
+            } else {
+                trueWorker = true;
+            }
+
+            if (vals.priority !== ""){
+                truePriority = todo.priority === vals.priority;
+            } else {
+                truePriority = true;
+            }
+
+            if (trueStatus && trueWorker && truePriority){
+                return true;
+            } else {
+                return false;
+            }
+        }).map(function({title, sugg_worker, index}){
+            return createCards(title, sugg_worker, index)
+        });
+        
+        return data;
+    }
+
+    function createCards(title, sugg_worker, index){
         return (
-            todos.map((todo, index) => (
-                <Grid item xs={4} key={index}>
-                    <Card style={ styles.card } >
-                        <div style={styles.details}>
-                            <CardContent style={styles.content}>
-                                <Container>
-                                    <Typography onClick={() => handleClickOpen(index)} component="h4" variant="h4">
-                                        {todo.title}
-                                    </Typography>
-                                
-                                    <Typography variant="subtitle1" color="textSecondary">
-                                        Assigned: {users[todo.sugg_worker]}
-                                    </Typography>
-                                </Container>
-                            </CardContent>
-                        </div>
-                    </Card>
-                </Grid>
-            ))
-        )
+            <Grid item xs={4} key={index}>
+                <Card style={ styles.card } >
+                    <div style={styles.details}>
+                        <CardContent style={styles.content}>
+                            <Container>
+                                <Typography onClick={() => handleClickOpen(index)} component="h4" variant="h4">
+                                    {title}
+                                </Typography>
+                            
+                                <Typography variant="subtitle1" color="textSecondary">
+                                    Assigned: {users[sugg_worker]}
+                                </Typography>
+                            </Container>
+                        </CardContent>
+                    </div>
+                </Card>
+            </Grid>
+        );
     }
 
     return (
@@ -141,7 +191,7 @@ export default function TodoGrid(props) {
                 </Container>
                 <Container maxWidth="lg">
                     <Grid container spacing={3} style={styles.root} direction="row" justify="center" alignItems="center">
-                        <CardFill />
+                        <CardFill status = { status } sugg_worker = { worker } priority = { priority } />
                     </Grid>
                 </Container>
                     <TodoDialog open={ open } onClose={ handleClose } data={ data } users={ users } history={ props.history } disabled = { disabled } add = { add ? 1 : 0} />
